cc52d69c8e6dfff5c2977a12d3c5d64303329c60
Geoffrey Lessel <code@geoffreylessel.com>
2023-08-11 13:12:12 -0500

    feat: update Blog.image/1 to take html opts to pass on

--- lib/blog.ex
+++ lib/blog.ex
@@ -1,7 +1,9 @@
 defmodule Blog do
-  def image(path) do
+  def image(path, html_opts \\ []) do
+    opts = Enum.map(html_opts, fn {key, value} -> "#{key}=#{value}" end)
+
     """
-    <img src="#{path}" />
+    <img src="#{path}" #{opts} />
     """
   end
 
